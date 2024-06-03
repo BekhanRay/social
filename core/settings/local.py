7315cff3013aad0fd9fadf33250d4ba6f5d0d7e4
BekhanRay <bekhanrayamjanov@gmail.com>
2024-06-03 17:31:52 +0600

    Initial commit Base of project

---
+++ core/settings/local.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
---
+++ rm_migrations.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+src_directory="apps"
+
+# Найти все поддиректории с именем migrations внутри директории src
+migrations_directories=$(find "$src_directory" -type d -name migrations)
+
+for dir in $migrations_directories;do
+    echo "Deleting migrations in $dir"
+    
+    # Удалить все файлы с расширением .py, кроме __init__.py
+    find "$dir" -type f -name "*.py" -not -name "__init__.py" -delete
+    
+    # Удалить все файлы с расширением .pyc
+    find "$dir" -type f -name "*.pyc" -delete
+done
+
+echo "Migrations deleted successfully."
