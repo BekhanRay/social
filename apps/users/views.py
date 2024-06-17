8ae3a736d9b525e64108ce6a6c89ab060f814976
BekhanRay <bekhanrayamjanov@gmail.com>
2024-06-17 13:56:57 +0600

    login hot fix

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,6 +1,7 @@
-from django.contrib.auth import login as auth_login
+from django.contrib.auth import login as auth_login, authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
+from django.core.checks import messages
 from django.shortcuts import render, redirect
 from django.utils import timezone
 
@@ -26,17 +27,32 @@ def register(request):
     return render(request, 'register.html')
 
 
+# def login_view(request):
+#     if request.method == 'POST':
+#         user = CustomUser.objects.get(login=request.POST['login'], password=request.POST['password'])
+#         if user is not None:
+#             auth_login(request, user)
+#             return redirect('profile')  # Перенаправление на страницу профиля
+#     else:
+#         pass
+#     return render(request, 'login.html')
 def login_view(request):
     if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
+        form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
-            user = form.get_user()
-            auth_login(request, user)
-            return redirect('profile')  # Перенаправление на страницу профиля
+            login = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=login, password=password)
+            if user is not None:
+                auth_login(request, user)
+                return redirect('profile')  # Перенаправление на страницу профиля
+            else:
+                messages.error(request, "Неверный логин или пароль.")
+        else:
+            messages.error(request, "Неверный логин или пароль.")
     else:
         form = AuthenticationForm()
     return render(request, 'login.html', {'form': form})
-
 
 @login_required
 def profile(request):
